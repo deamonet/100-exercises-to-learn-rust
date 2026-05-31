8040872c73610e167b861e4071f2a14ad0c66f6e
deamonet <deamonet@outlook.com>
2026-06-01 05:34:05 +0800

    finished

--- exercises/03_ticket_v1/02_validation/src/lib.rs
+++ exercises/03_ticket_v1/02_validation/src/lib.rs
@@ -18,7 +18,7 @@ impl Ticket {
     // as well as some `String` methods. Use the documentation of Rust's standard library
     // to find the most appropriate options -> https://doc.rust-lang.org/std/string/struct.String.html
     fn new(title: String, description: String, status: String) -> Self {
-        if status != "To-Do" || status != "In Progress" || status != "Done"  {
+        if status != "To-Do" && status != "In Progress" && status != "Done"  {
             panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
         }
         if title.is_empty(){
