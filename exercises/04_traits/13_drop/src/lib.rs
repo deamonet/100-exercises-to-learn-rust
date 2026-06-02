c4bd5736bfa1cc07b3a133788379217f9d1cc6d0
deamonet <deamonet@outlook.com>
2026-06-03 02:38:11 +0800

    4-13

--- exercises/04_traits/13_drop/src/lib.rs
+++ exercises/04_traits/13_drop/src/lib.rs
@@ -2,6 +2,29 @@
 //  unless a certain operation has been performed on it.
 //  You can see the expected API in the tests below.
 
+struct DropBomb {
+    is_defuse: bool,
+}
+
+impl DropBomb {
+    fn new() -> Self {
+        DropBomb { is_defuse: false }
+    }
+
+    fn defuse(&mut self) {
+        self.is_defuse = true;
+    }
+}
+
+impl Drop for DropBomb {
+    fn drop(&mut self) {
+        if self.is_defuse {
+            return;
+        }
+        panic!("defuse is false");
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
