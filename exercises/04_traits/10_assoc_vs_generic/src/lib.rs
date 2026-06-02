1ab26c53e03dbaef01a47b668cc91b6b8b81cfa8
deamonet <deamonet@outlook.com>
2026-06-03 02:16:19 +0800

    4-10

--- exercises/04_traits/10_assoc_vs_generic/src/lib.rs
+++ exercises/04_traits/10_assoc_vs_generic/src/lib.rs
@@ -13,6 +13,28 @@
 // You don't have to though: it's perfectly okay to write three separate
 // implementations manually. Venture further only if you're curious.
 
+trait Power<T> {
+    fn power(&self, exponential: T) -> u32;
+}
+
+impl Power<u32> for u32 {
+    fn power(&self, exponential: u32) -> u32 {
+        *&self.pow(exponential)
+    }
+}
+
+impl Power<u16> for u32 {
+    fn power(&self, exponential: u16) -> u32 {
+        *&self.pow(exponential as u32)
+    }
+}
+
+impl Power<&u32> for u32 {
+    fn power(&self, exponential: &u32) -> u32 {
+        *&self.pow(*exponential)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::Power;
