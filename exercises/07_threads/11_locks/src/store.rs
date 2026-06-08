84104d6a67ffb92155c4e3dc0a033d5c97134080
deamonet <deamonet@outlook.com>
2026-06-08 18:06:33 +0800

    7-11

--- exercises/07_threads/11_locks/src/store.rs
+++ exercises/07_threads/11_locks/src/store.rs
@@ -28,13 +28,17 @@ impl TicketStore {
             description: ticket.description,
             status: Status::ToDo,
         };
-        todo!();
+        let arc = Arc::new(Mutex::new(ticket));
+        self.tickets.insert(id, arc);
         id
     }
 
     // The `get` method should return a handle to the ticket
     // which allows the caller to either read or modify the ticket.
-    pub fn get(&self, id: TicketId) -> Option<todo!()> {
-        todo!()
+    pub fn get(&self, id: TicketId) -> Option<Arc<Mutex<Ticket>>> {
+        match self.tickets.get(&id) {
+            Some(arc) => Some(arc.clone()),
+            None => None,
+        }
     }
 }
